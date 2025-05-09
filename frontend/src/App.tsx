a1387ccc962aecd5223fe5e82583e34079c377ce
declan <declan@libecc.io>
2025-05-09 14:27:20 +0800

    manage page url change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,7 +18,7 @@ const App: React.FC = () => {
           <Routes>
             <Route path="/login" element={<LoginPage />} />
             <Route
-              path="/shortage-manage"
+              path="/manage/shortage"
               element={
                 <ProtectedRoute>
                   <ShortageManagePage />
