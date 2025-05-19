54abbe8dce03ed61167945c67d16140c8fa2c8fd
declan <declan@libecc.io>
2025-05-19 09:52:52 +0800

    handle sigin silent

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { Spin } from "@arco-design/web-react";
 import { useAuth } from "react-oidc-context";
@@ -8,25 +8,39 @@ interface ProtectedRouteProps {
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  const { isAuthenticated, isLoading, error } = useAuth();
+  const { signinSilent, isAuthenticated, isLoading, error } = useAuth();
+  const [triedSigninSilent, setTriedSigninSilent] = useState(false);
 
-  if (isLoading) {
+  useEffect(() => {
+    if (isLoading) {
+      return;
+    }
+    if (isAuthenticated) {
+      return;
+    }
+    if (signinSilent && !triedSigninSilent) {
+      setTriedSigninSilent(true);
+      signinSilent?.({ scope: "email sub" });
+    }
+  }, [signinSilent, triedSigninSilent, setTriedSigninSilent, isLoading, isAuthenticated]);
+
+  if (isAuthenticated) {
+    return <>{children}</>;
+  }
+
+  if (error) {
+    return <div>发生错误: {error.message}</div>;
+  }
+
+  if (isLoading || !triedSigninSilent) {
     return (
       <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
         <Spin size={128} tip="加载中..." />
       </div>
     );
   }
-  
-  if (error) {
-    return <div>发生错误: {error.message}</div>;
-  }
 
-  if (!isAuthenticated) {
-    return <Navigate to="/login" replace />;
-  }
-
-  return <>{children}</>;
+  return <Navigate to="/login" replace />;
 };
 
 export default ProtectedRoute;
